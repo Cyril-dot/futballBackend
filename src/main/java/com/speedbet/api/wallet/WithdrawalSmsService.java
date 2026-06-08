637c833ca76e6ee3d668c6b6eff21ed4e3fac12e
Ottih ChuksCyril-dot <chukwukaottih@gmail.com>
2026-06-08 14:14:57 +0000

    update

--- src/main/java/com/speedbet/api/wallet/WithdrawalSmsService.java
+++ src/main/java/com/speedbet/api/wallet/WithdrawalSmsService.java
@@ -1,6 +1,5 @@
 package com.speedbet.api.wallet;
 
-import com.speedbet.api.sms.ArkeselSmsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
