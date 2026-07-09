5b584f116591d15db159c9a2c46e8fc02eeb4f39
Cyril-dot <chukwukaottih@gmail.com>
2026-07-09 07:40:40 +0000

    upadte

--- src/main/java/com/speedbet/api/config/SecurityConfig.java
+++ src/main/java/com/speedbet/api/config/SecurityConfig.java
@@ -337,6 +337,9 @@ public class SecurityConfig {
                 "https://www.nexbett.xyz",
                 "https://nex-bet-pink.vercel.app",
                 "https://nex-bet-admin.vercel.app",
+                "https://crown-bet-xi.vercel.app",
+                "https://crownbett.xyz",
+                "https://www.crownbett.xyz",
                 "https://crown-bet-xi.vercel.app"
 
         ));
